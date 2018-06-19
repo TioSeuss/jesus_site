87b294f983b704ce54b09ce8c0a0f8224eb74156
jesus <jesus@codegiant.io>
2018-06-19 18:09:01 +0700

    another footer update

--- wp-content/themes/rocked/footer.php
+++ wp-content/themes/rocked/footer.php
@@ -16,13 +16,13 @@
 		<?php get_sidebar('footer'); ?>
 	<?php endif; ?>
 
-	<!--<footer id="colophon" class="site-footer" role="contentinfo">
+	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info container">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'rocked' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'rocked' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %2$s by %1$s.', 'rocked' ), 'aThemes', '<a href="http://athemes.com/theme/rocked" rel="designer">Rocked</a>' ); ?>
-		</div> .site-info 
-	</footer> #colophon -->
+			<p>
+				<?php _e('© 2018 Valyrian Coffee, All Rights Reserved'); ?>
+			</p>
+		</div> <!--.site-info -->
+	</footer><!-- #colophon -->
 </div><!-- #page -->
 <a class="go-top">
 	<i class="fa fa-angle-up"></i>
